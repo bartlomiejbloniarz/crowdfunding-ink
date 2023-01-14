305838d890e5900df15bb6a72aee374f732bc8bc
bartlomiejbloniarz <bartlomiej.bloniarz@student.uj.edu.pl>
2023-01-14 23:41:28 +0100

    Add router

---
+++ front/src/components/NoPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {AppLayout, Header} from "@cloudscape-design/components";
+
+const NoPage = () => {
+    return (
+        <AppLayout
+            toolsHide={true}
+            navigationHide={true}
+            contentHeader={
+                <Header
+                    variant={"h1"}
+                >
+                    404 Page not found
+                </Header>
+            }
+        />
+    )
+}
+
+export default NoPage
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "@cloudscape-design/global-styles/index.css"
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NoPage from "./components/NoPage";
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/">
+                <Route index element={<App/>}/>
+                <Route path="*" element={<NoPage />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
 );
 
